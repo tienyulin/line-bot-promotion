4736a699b15c613d05442610766c4be5e2764803
tienyulin <t.tienyulin@gmail.com>
2022-04-20 22:06:09 +0800

    feat: add filter message

--- app/services/event_service.rb
+++ app/services/event_service.rb
@@ -15,7 +15,7 @@ module TienYuBot
           when Line::Bot::Event::MessageType::Text
             message = {
               type: 'text',
-              text: event.message['text']
+              text: '您好，我是林天佑，請透過下方選單進行點選，我將向您介紹關於我的經歷和能力。'
             }
 
             EventRepository.reply_message(client, event['replyToken'], message)
@@ -31,14 +31,35 @@ module TienYuBot
         when Line::Bot::Event::Message
           case event.type
           when Line::Bot::Event::MessageType::Text
-            introduction = File.read('app/sources/message/introduction.json')
-            message = JSON.parse(introduction)
+            message = filter_message(event.message['text'])
 
             user_id = event['source']['userId']
             EventRepository.push_message(client, user_id, message)
           end
         end
       end
+    end
+
+    def filter_message(text)
+      message_file = nil
+      case text
+      when text.include?('自我介紹')
+        message_file = File.read('app/sources/message/introduction.json')
+      when text.include?('工作經驗')
+        message_file = File.read('app/sources/message/work.json')
+      when text.include?('大學')
+        message_file = File.read('app/sources/message/education.json')
+      when text.include?('專案')
+        message_file = File.read('app/sources/message/projects.json')
+      when text.include?('能力')
+        message_file = File.read('app/sources/message/skills.json')
+      when text.include?('人格特質')
+        message_file = File.read('app/sources/message/personality.json')
+      else
+        return { type: 'text', text: '您好，我是林天佑，請透過下方選單進行點選，我將向您介紹關於我的經歷和能力。' }
+      end
+
+      JSON.parse(message_file)
     end
   end
 end

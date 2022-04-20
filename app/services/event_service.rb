b30a62566e93570826c3c47eaf6a5b6dbb06633e
tienyulin <t.tienyulin@gmail.com>
2022-04-20 22:17:14 +0800

    fix: switch case when to if else

--- app/services/event_service.rb
+++ app/services/event_service.rb
@@ -43,18 +43,18 @@ module TienYuBot
     private
     def self.filter_message(text)
       message_file = nil
-      case text
-      when text.include?('自我介紹')
+
+      if text.include?('自我介紹')
         message_file = File.read('app/sources/message/introduction.json')
-      when text.include?('工作經驗')
+      elsif text.include?('工作經驗')
         message_file = File.read('app/sources/message/work.json')
-      when text.include?('大學')
+      elsif text.include?('大學')
         message_file = File.read('app/sources/message/education.json')
-      when text.include?('專案')
+      elsif text.include?('專案')
         message_file = File.read('app/sources/message/projects.json')
-      when text.include?('能力')
+      elsif text.include?('能力')
         message_file = File.read('app/sources/message/skills.json')
-      when text.include?('人格特質')
+      elsif text.include?('人格特質')
         message_file = File.read('app/sources/message/personality.json')
       else
         return { type: 'text', text: '您好，我是林天佑，請透過下方選單進行點選，我將向您介紹關於我的經歷和能力。' }
